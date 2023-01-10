647cd90746601ad5804eb98b302495bee3f74ad6
Jeremy Gray <jgray@brainstation.io>
2023-01-09 21:28:19 -0500

    changed layout

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,14 @@
-function reset(){
+function newGame(){
     document.getElementById('playerLP').value = document.getElementById('playerLP').defaultValue;
     document.getElementById('oppLP').value = document.getElementById('oppLP').defaultValue;
+}
+
+function newMatch(){
+    newGame();
+    document.getElementById('playerWins').value = document.getElementById('playerWins').defaultValue;
+    document.getElementById('oppWins').value = document.getElementById('oppWins').defaultValue;
+
+
 }
 
 function increment(param, step){
