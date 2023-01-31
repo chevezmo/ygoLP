db2a01979725b43db6bf05efb5a2d6b0fa3f3402
Jeremy Gray <jgray@brainstation.io>
2023-01-31 00:03:03 -0500

    added notes

--- js/functions.js
+++ js/functions.js
@@ -1,36 +1,54 @@
 function newGame(){
     document.getElementById('playerLP').value = document.getElementById('playerLP').defaultValue;
     document.getElementById('oppLP').value = document.getElementById('oppLP').defaultValue;
-    document.getElementById("playerLog").innerHTML = "";
-    document.getElementById("oppLog").innerHTML = "";
+    document.getElementById("playerLog").innerHTML = "8000";
+    document.getElementById("oppLog").innerHTML = "8000";
 }
 
 function newMatch(){
     newGame();
     document.getElementById('playerWins').value = document.getElementById('playerWins').defaultValue;
     document.getElementById('oppWins').value = document.getElementById('oppWins').defaultValue;
-
-
 }
 
-function increment(param){
-    let LP = parseInt(document.getElementById(`${param}`).value);
-    let gain = parseInt(document.getElementById('stepSize').value) || 0;
-    document.getElementById(`${param}`).value = LP + gain;
-    log(param, LP, document.getElementById(`${param}`).value);
+function increment(player){
+    document.getElementById(`${player}`).stepUp();
 }
 
-function decrement(param){
-    let LP = parseInt(document.getElementById(`${param}`).value);
-    let loss = parseInt(document.getElementById('stepSize').value) || 0;
-    document.getElementById(`${param}`).value = LP - loss;
-    log(param, LP, document.getElementById(`${param}`).value);
+function buttonChange(){
+    switch(document.getElementById('buttonLPchange').innerHTML){
+        case '⚔️':
+            document.getElementById('buttonLPchange').innerHTML = '❤️';
+            break;
+        case '❤️':
+            document.getElementById('buttonLPchange').innerHTML = '⚔️';
+            break;
+    }
 }
 
-function log(param, previous, damage){
-    if(param == "playerLP"){
-        document.getElementById("playerLog").innerHTML = document.getElementById("playerLog").innerHTML + "<br />" + previous + " -> " + damage;
-    } else {
-        document.getElementById("oppLog").innerHTML = document.getElementById("oppLog").innerHTML + "<br />" + previous + " -> " + damage;
+function showNotes(){
+    switch(document.getElementById('notes').style.display){
+        case 'block':
+            document.getElementById('notes').style.display = 'none';
+            break;
+        default:
+            document.getElementById('notes').style.display = 'block';
+    }
+}
+
+function LPchange(player, log){
+    let LP = parseInt(document.getElementById(`${player}`).value);
+    let change = parseInt(document.getElementById('stepSize').value) || 0;
+    switch(document.getElementById('buttonLPchange').innerHTML){
+        case '❤️':
+            document.getElementById(`${player}`).value = LP + change;
+            document.getElementById(`${log}`).innerHTML = document.getElementById(`${log}`).innerHTML + "<br />" + change + " ❤️ "+ (LP + change);
+            break;
+        case '⚔️':
+            document.getElementById(`${player}`).value = LP - change;
+            document.getElementById(`${log}`).innerHTML = document.getElementById(`${log}`).innerHTML + "<br />" + change + " ⚔️ "+ (LP - change) ;
+            break;
+        default:
+            document.getElementById(`${player}`).value;
     }
 }
